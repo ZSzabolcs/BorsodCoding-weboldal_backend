6644346322ad2ef3011a388aa6346a63f524c0ed
Cs12krisz <csigerkrisztian@gmail.com>
2025-10-23 00:03:16 +0200

    Async metódussal ellenőrizzük a táblák adatai fent vannak-e és ha nem, akkor feltöltjük

--- For_The_Potatoe_Backend/Models/For_The_PotatoeDbContext.cs
+++ For_The_Potatoe_Backend/Models/For_The_PotatoeDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace For_The_Potatoe_Backend.Models
 {
@@ -18,8 +19,61 @@ namespace For_The_Potatoe_Backend.Models
 
         public DbSet<SaveColumns> Save { get; set; }
 
+
+        public static async Task SeedDataAsync(IServiceProvider serviceProvider)
+        {
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<For_The_PotatoeDbContext>();
+
+                await context.Database.MigrateAsync();
+
+                if (!context.User.Any()) // ELLENŐRZÉS
+                {
+                    using (StreamReader sr = new StreamReader("UserData.csv"))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            var sor = sr.ReadLine().Split(';');
+                            context.User.Add(new UserColumns
+                            {
+                                Id = int.Parse(sor[0]),
+                                Name = sor[1],
+                                Password = sor[2],
+                                Date = DateTime.Parse(sor[3])
+                            });
+                        }
+
+                    }
+
+                    using (StreamReader sr = new StreamReader("SaveData.csv"))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            var sor = sr.ReadLine().Split(',');
+                            context.Save.Add(new SaveColumns
+                            {
+                                Id = int.Parse(sor[0]),
+                                Points = int.Parse(sor[1]),
+                                Level = int.Parse(sor[2]),
+                                Language = sor[3],
+                                Date = DateTime.Parse(sor[4]),
+                                UserId = int.Parse(sor[0])
+                            });
+                        }
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+            }
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+
+            modelBuilder.Entity<UserColumns>()
+                .HasIndex(e => e.Name)
+                .IsUnique();
 
             modelBuilder.Entity<UserColumns>()
                 .HasMany(e => e.Saves)
