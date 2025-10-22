6ed738504b5733361069a7c513a3a51db4f7a9ef
Cs12krisz <csigerkrisztian@gmail.com>
2025-10-22 10:12:05 +0200

    User tábla osztálya, azaz oszlopai

---
+++ For_The_Potatoe_Backend/Models/UserColumns.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using For_The_Potatoe_Backend.Models;
+
+namespace For_The_Potatoe_Backend.Models
+{
+    public class UserColumns
+    {
+        [Key]
+        public  int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public DateTime Date { get; set; }
+
+        public ICollection<SaveColumns> Saves { get; }
+    }
+}
