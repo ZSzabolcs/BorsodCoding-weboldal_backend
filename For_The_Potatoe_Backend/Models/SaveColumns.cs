6755e5254206bff65b916414ad87c289e0b1fb48
Cs12krisz <csigerkrisztian@gmail.com>
2025-10-22 10:10:10 +0200

    Save tábla osztálya, azaz oszlopai

---
+++ For_The_Potatoe_Backend/Models/SaveColumns.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using For_The_Potatoe_Backend.Models;
+
+namespace For_The_Potatoe_Backend.Models
+{
+    public class SaveColumns
+    {
+        public int Id { get; set; }
+
+        public int Points { get; set; }
+
+        public int Level { get; set; }
+
+        public string Language { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int UserId { get; set; }
+
+        public UserColumns UserColumns { get; set; }
+    }
+}
