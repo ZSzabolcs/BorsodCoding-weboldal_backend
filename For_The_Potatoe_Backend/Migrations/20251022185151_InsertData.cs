3c7a677abc31e91dfcabbf5c63289aa5f217c024
Cs12krisz <csigerkrisztian@gmail.com>
2025-10-22 22:54:23 +0200

    Egy migráns a két tábla adatainak feltöltéséhez

---
+++ For_The_Potatoe_Backend/Migrations/20251022185151_InsertData.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using For_The_Potatoe_Backend;
+using MySqlX.XDevAPI.CRUD;
+
+#nullable disable
+
+namespace For_The_Potatoe_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            using (StreamReader sr = new StreamReader("UserData.csv"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var sor = sr.ReadLine().Split(';');
+                    migrationBuilder.InsertData(
+                    table: "User",
+                    columns: ["Id", "Name", "Password", "Date"],
+                    values: [int.Parse(sor[0]), sor[1], sor[2], sor[3]]
+                    );
+                }
+
+            }
+
+            using (StreamReader sr = new StreamReader("SaveData.csv"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var sor = sr.ReadLine().Split(',');
+                    migrationBuilder.InsertData(
+                    table: "Save",
+                    columns: ["Id", "Points", "Level", "Language", "Date", "UserId"],
+                    values: [int.Parse(sor[0]), sor[1], sor[2], sor[3], sor[4], int.Parse(sor[0])]
+                    );
+                }
+            }
+            
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
