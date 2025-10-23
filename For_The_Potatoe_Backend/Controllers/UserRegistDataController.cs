fa82a8eea1e4d02486ff834828a044544db82777
Cs12krisz <csigerkrisztian@gmail.com>
2025-10-23 13:57:08 +0200

    A Save tábla Controllerje

---
+++ For_The_Potatoe_Backend/Controllers/UserRegistDataController.cs
@@ -0,0 +1,30 @@
+﻿using For_The_Potatoe_Backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using For_The_Potatoe_Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace For_The_Potatoe_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserRegistData : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<UserColumns> GetAllData()
+        {
+            using (var context = new For_The_PotatoeDbContext())
+            {
+                var users = context.User.ToList();
+
+
+                if (users != null)
+                {
+                    return Ok(users);
+                }
+                return BadRequest(new { message = "Sikertelen lekérdezés" });
+            }
+
+        }
+    }
+}
---
+++ For_The_Potatoe_Backend/Controllers/UserSaveDataController.cs
@@ -0,0 +1,28 @@
+﻿using For_The_Potatoe_Backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace For_The_Potatoe_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserSaveData : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<SaveColumns> GetAllData()
+        {
+            using (var context = new For_The_PotatoeDbContext())
+            {
+                var users = context.Save.ToList();
+
+
+                if (users != null)
+                {
+                    return Ok(users);
+                }
+                return BadRequest(new { message = "Sikertelen lekérdezés" });
+            }
+
+        }
+    }
+}
