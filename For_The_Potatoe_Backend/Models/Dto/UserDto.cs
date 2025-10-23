47fd9c2a655e9f1384da2d95ddaa08bcf617dbdb
Cs12krisz <csigerkrisztian@gmail.com>
2025-10-24 00:59:33 +0200

    UserDto osztály az User tábla feltöltéséhez

---
+++ For_The_Potatoe_Backend/Models/Dto/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace For_The_Potatoe_Backend.Models.Dto
+{
+    public class UserDto
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
