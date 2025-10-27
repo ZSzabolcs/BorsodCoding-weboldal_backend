a85841e2993784c564549ad0d077ed91254e64ac
Cs12krisz <csigerkrisztian@gmail.com>
2025-10-27 20:00:10 +0100

    A Language mező értékének ellenőrzése

--- For_The_Potatoe_Backend/Models/Dto/InsertSaveDto.cs
+++ For_The_Potatoe_Backend/Models/Dto/InsertSaveDto.cs
@@ -4,12 +4,19 @@ namespace For_The_Potatoe_Backend.Models.Dto
 {
     public class InsertSaveDto
     {
+        private string language;
+
         public string Name { get; set; }
         public int Points { get; set; }
 
         public int Level { get; set; }
 
-        public string Language { get; set; }
+        public string Language 
+        { 
+            get { return language; }
+            set { if (value == "hu" || value == "en") language = value; }
+
+        }
 
         public DateTime Date { get; set; } = DateTime.Now;
 
